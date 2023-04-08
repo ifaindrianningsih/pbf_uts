a1f050b1cc81f4c831e845494b431d6a46c53e5c
ifaindrianningsih <ifaindrian152@gmail.com>
2023-04-08 11:58:03 +0700

    first commit

---
+++ src/container/Peternakan/Karyawan.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Karyawan= () => {
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-title">
+                    <h2>Daftar Karyawan</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Karyawan;
---
+++ src/container/Peternakan/Obat.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Obat= () => {
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-title">
+                    <h2>List Obat</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Obat;
